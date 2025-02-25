6ef3f4702b6c8baf7f7cecb60f060dee56a93dd7
Velsimir <nintendocoin@yandex.ru>
2025-02-25 16:28:43 +0300

    Camera logic was changed

--- Assets/CodeBase/CameraLogic.cs
+++ Assets/CodeBase/CameraLogic.cs
@@ -4,14 +4,12 @@ namespace CodeBase
 {
     public class CameraLogic : MonoBehaviour
     {
-        [SerializeField] private float moveSpeed = 10f;
-        [SerializeField] private float edgeScrollSize = 20f;
+        [SerializeField] private float _moveSpeed = 10f;
+        [SerializeField] private float _edgeScrollSize = 20f;
 
         private void Update()
         {
             Vector3 moveDirection = Vector3.zero;
-
-            moveDirection = TakeDirectionByArrows(moveDirection);
 
             moveDirection = TakeDirectionByMouse(moveDirection);
 
@@ -20,24 +18,16 @@ namespace CodeBase
             Vector3 globalMoveDirection = transform.TransformDirection(moveDirection);
             globalMoveDirection.y = 0;
 
-            transform.Translate(globalMoveDirection * moveSpeed * Time.deltaTime, Space.World);
+            transform.Translate(globalMoveDirection * _moveSpeed * Time.deltaTime, Space.World);
         }
 
         private Vector3 TakeDirectionByMouse(Vector3 moveDirection)
         {
-            if (Input.mousePosition.x <= edgeScrollSize) moveDirection.x -= 1;
-            if (Input.mousePosition.x >= Screen.width - edgeScrollSize) moveDirection.x += 1;
-            if (Input.mousePosition.y <= edgeScrollSize) moveDirection.z -= 1;
-            if (Input.mousePosition.y >= Screen.height - edgeScrollSize) moveDirection.z += 1;
-            return moveDirection;
-        }
-
-        private static Vector3 TakeDirectionByArrows(Vector3 moveDirection)
-        {
-            if (Input.GetKey(KeyCode.W)) moveDirection.z += 1;
-            if (Input.GetKey(KeyCode.S)) moveDirection.z -= 1;
-            if (Input.GetKey(KeyCode.A)) moveDirection.x -= 1;
-            if (Input.GetKey(KeyCode.D)) moveDirection.x += 1;
+            if (Input.mousePosition.x <= _edgeScrollSize) moveDirection.x -= 1;
+            if (Input.mousePosition.x >= Screen.width - _edgeScrollSize) moveDirection.x += 1;
+            if (Input.mousePosition.y <= _edgeScrollSize) moveDirection.z -= 1;
+            if (Input.mousePosition.y >= Screen.height - _edgeScrollSize) moveDirection.z += 1;
+            
             return moveDirection;
         }
     }
